4b135bb8a73f30fe1a2f879d382c2bfd6d24e534
Michael Kwan <michaelk.kwan@mail.utoronto.ca>
2020-08-12 00:48:29 -0400

    Get HTML from course webpage

---
+++ webscrape/GetCourseInformation.py
@@ -0,0 +1,40 @@
+import requests  # get the html from websites
+from bs4 import BeautifulSoup
+
+csc_link = "https://student.utm.utoronto.ca/calendar/list_courses.pl?Depart=7"
+mat_link = "https://student.utm.utoronto.ca/calendar/list_courses.pl?Depart=27"
+sta_link = "https://student.utm.utoronto.ca/calendar/list_courses.pl?Depart=38"
+
+# Store request objects for each of the links 
+request_csc = requests.get(csc_link)
+request_mat = requests.get(mat_link)
+request_sta = requests.get(sta_link)
+
+# Make BeautifulSoup objects for each of the course subjects
+csc_soup = BeautifulSoup(request_csc.text, 'html.parser')
+mat_soup = BeautifulSoup(request_mat.text, 'html.parser')
+sta_soup = BeautifulSoup(request_sta.text, 'html.parser')
+
+
+def Output_Text_Data(s1: BeautifulSoup, s2: BeautifulSoup, s3: BeautifulSoup) -> None:
+    """
+    Takes the 3 BeautifulSoup objects and output the html text in a prettyfied
+    format and outputs it into a text file
+    """
+    f = open("html_output", "w")
+    f.write(s1.prettify + "\n")
+    f.write(s2.prettify + "\n")
+    f.write(s3.prettify + "\n")
+    f.close
+
+def Print_Text_Data(s1: BeautifulSoup, s2: BeautifulSoup, s3: BeautifulSoup) -> None:
+    """
+    Print the HTML data from the 3 BeautifulSoup objects
+    in a prettyfied format to the console
+    """
+    print(csc_soup.prettify, "\n **END OF CSC**")
+    print(mat_soup.prettify, "\n **END OF MAT**")
+    print(sta_soup.prettify, "\n **END OF STA**\n")
+
+if __name__ == "__main__":
+    pass
